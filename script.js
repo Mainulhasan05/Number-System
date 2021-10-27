cd738d617ef5226b9954283e0040099fc182a5de
Mainulhasan05 <mdrifatbd5@gmail.com>
2021-10-27 19:12:04 +0600

    msg

--- script.js
+++ script.js
@@ -17,3 +17,71 @@ function decimal(){
     hexadecimal.value=hexValue.toString().toUpperCase();
 
 }
+
+function binary(){
+    const deci=document.getElementById('decimal');
+    const binary=document.getElementById('binary');
+    const octal=document.getElementById('octal');
+    const hexadecimal=document.getElementById('hexadecimal');
+    let x=binary.value;
+    let myArr=x.split('.');   
+    let size=myArr[0].length;
+    let text=String(myArr[0]);
+    let valid=0;
+    for(let i=0; i<size; i++){
+        if(text[i]==1 || text[i]==0){
+            
+        }
+        else{
+            valid=1;
+            break;
+        }
+    }
+    if(valid){
+        deci.value='dsd'/2;
+    }
+    else{
+        deci.value=parseInt(myArr[0], 2); 
+    }
+   
+    if(myArr.length>1){
+        let p=myArr[1];
+        let sz=String(p).length;
+        let check=0;
+        for(let j=0; j<sz; j++){
+            if(p[j]=='1' || p[j]=='0'){
+
+            }
+            else{
+                check=1;
+                break;
+            }
+        }
+        
+        if(check==0){
+            let sum=0;
+            p=String(p);
+            console.log(sz);
+            for(let i=sz; i>=1;i--){
+                sum=sum+parseFloat(p[i-1])/Math.pow(2,i);
+                
+            }
+            sum=parseFloat(sum);
+            deci.value=Number(deci.value)+Number(sum);
+        
+        }else{
+            deci.value='dfd'/3;
+        }
+
+    }
+    let lala=Number(deci.value);
+    let ocValue=lala.toString(8);
+    let hexValue=lala.toString(16);
+
+    octal.value=ocValue;
+    hexadecimal.value=hexValue.toString().toUpperCase();
+
+
+    
+
+}
